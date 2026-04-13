155b0d93552a96477dedb0ee1db5fef345f84f3b
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-13 19:37:36 -0300

    enums terminado

---
+++ src/BookStore.Domain/Enums/BookFormat.cs
@@ -0,0 +1,16 @@
+﻿namespace BookStore.Domain.Enums;
+
+public enum BookFormat
+{
+    Paperback = 1,
+    Hardcover = 2,
+    Ebook = 3,
+    Audiobook = 4
+}
+
+public enum CustomerRole
+{
+    Customer = 1,
+    Admin = 2
+}
+
---
+++ src/BookStore.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Domain.Enums;
+
+public enum OrderStatus
+{
+    Pending = 1,
+    PaymentConfirmed = 2,
+    Processing = 3,
+    Shipped = 4,
+    Delivered = 5,
+    Cancelled = 6,
+    Returned = 7
+}
