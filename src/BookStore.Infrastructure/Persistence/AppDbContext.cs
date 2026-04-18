eae88f3a59266401c79f54034197fa043d52027a
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-18 20:37:07 -0300

    appdbcontext :)

--- src/BookStore.Infrastructure/Persistence/AppDbContext.cs
+++ src/BookStore.Infrastructure/Persistence/AppDbContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace BookStore.Infrastructure.Persistence
+namespace BookStore.Infrastructure.Persistence;
+
+public sealed class AppDbContext : DbContext
 {
-    internal class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<Cart> Carts => Set<Cart>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
     }
 }
