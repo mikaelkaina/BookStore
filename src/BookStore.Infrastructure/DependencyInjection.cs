b8cfc1f8896b62046ce9903e24dbb8413be80b7b
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-19 17:13:14 -0300

    DI de infrastructure

---
+++ src/BookStore.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Infrastructure.Interceptors;
+using BookStore.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore.Infrastructure;
+
+public static class DependencyInjection 
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<DomainEventInterceptor>();
+
+        services.AddDbContext<AppDbContext>((sp, options ) =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                sql => sql.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+
+            options.AddInterceptors(sp.GetRequiredService<DomainEventInterceptor>());
+        });
+
+        return services;
+    }
+}
