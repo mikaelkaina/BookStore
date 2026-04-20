10a1042deaabbeb9486d10013fe710e4df8c5e07
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-19 23:41:41 -0300

    dando inicio nos respositories

--- src/BookStore.Domain/Interfaces/IBookRepository.cs
+++ src/BookStore.Domain/Interfaces/IBookRepository.cs
@@ -3,10 +3,11 @@ using BookStore.Domain.ValueObjects;
 
 namespace BookStore.Domain.Interfaces;
 
-public interface IBookRepository :IRepository<Book>
+public interface IBookRepository : IRepository<Book>
 {
+    Task<Book?> GetByIsbnAsync(Isbn isbn, CancellationToken cancellationToken = default);  
     Task<(IEnumerable<Book> Books, int TotalCount)> GetPagedAsync (
-        string? searchTerm, Guid? categoryId, decimal? minPrince, decimal? maxPrince, bool? sortyBy, 
+        string? searchTerm, Guid? categoryId, decimal? minPrince, decimal? maxPrince, bool? sortByPrice, 
         bool ascending, int page, int pageSize, CancellationToken cancellationToken = default);
 
     Task<bool> IsbnExistsAsync(Isbn isbn, Guid? excludeBookId = null, CancellationToken cancellationToken = default);
