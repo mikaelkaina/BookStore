eaa3870aa5ec4cb210bd9c405f43dde8aec5105b
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-20 12:50:28 -0300

    conluindo os respositories

---
+++ src/BookStore.Infrastructure/Persistence/Repositories/CartRepository.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Domain.Entities;
+using BookStore.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Infrastructure.Persistence.Repositories;
+
+public sealed class CartRepository : ICartRepository
+{
+    private readonly AppDbContext _context;
+    public CartRepository(AppDbContext context)
+        => _context = context;
+
+    public async Task AddAsync(Cart entity, CancellationToken cancellationToken = default)
+        => await _context.Carts.AddAsync(entity, cancellationToken);
+    public Task DeleteAsync(Cart entity, CancellationToken cancellationToken = default)
+    {
+        _context.Carts.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<Cart?> GetByCustomerIdAsync(Guid customerId, CancellationToken cancellationToken = default)
+        => await _context.Carts
+        .Include(c => c.Items)
+        .FirstOrDefaultAsync(c => 
+        c.CustomerId == customerId &&
+        !c.IsCheckedOut &&
+        c.ExpiresAt > DateTime.UtcNow, cancellationToken);
+
+    public async Task<Cart?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await _context.Carts
+        .Include(c => c.Items)
+        .FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+
+    public async Task<Cart?> GetBySessionIdAsync(string sessionId, CancellationToken cancellationToken)
+        => await _context.Carts
+        .Include(c => c.Items)
+        .FirstOrDefaultAsync(c => 
+        c.SessionId == sessionId &&
+        !c.IsCheckedOut &&
+        c.ExpiresAt > DateTime.UtcNow, cancellationToken);
+
+    public Task UpdateAsync(Cart entity, CancellationToken cancellationToken = default)
+    {
+        _context.Carts.Update(entity);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/BookStore.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Domain.Entities;
+using BookStore.Domain.Interfaces;
+using BookStore.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Infrastructure.Persistence.Repositories;
+
+public sealed class CustomerRepository : ICustomerRepository
+{
+    private readonly AppDbContext _context;
+    public CustomerRepository(AppDbContext context)
+        => _context = context;
+
+    public async Task AddAsync(Customer entity, CancellationToken cancellationToken = default)
+        => await _context.AddAsync(entity, cancellationToken);
+
+    public Task DeleteAsync(Customer entity, CancellationToken cancellationToken = default)
+    {
+        _context.Customers.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<bool> EmailExistsAsync(Email email, Guid? excludeCustomerId = null, CancellationToken cancellationToken = default)
+        => await _context.Customers
+        .AnyAsync(c =>
+            c.Email.Value == email.Value &&
+            (excludeCustomerId == null || c.Id != excludeCustomerId.Value),
+            cancellationToken);
+
+    public Task<Customer?> GetByEmailAsync(Email email, CancellationToken cancellationToken = default)
+        => _context.Customers.FirstOrDefaultAsync(c => c.Email.Value == email.Value, cancellationToken);
+
+    public Task<Customer?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => _context.Customers.FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+
+    public Task UpdateAsync(Customer entity, CancellationToken cancellationToken = default)
+    {
+        _context.Customers.Update(entity);
+        return Task.CompletedTask;
+    }
+}
