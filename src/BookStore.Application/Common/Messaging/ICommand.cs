d1289c4d16dbf84796ed2c2bf0e6f9a62ad2aec9
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-21 20:17:35 -0300

    dando incicio a camada de application

---
+++ src/BookStore.Application/Common/Messaging/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Application.Common.Messaging
+{
+    internal interface ICommand
+    {
+    }
+}
---
+++ src/BookStore.Application/Features/Books/BookMappingExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Application.Features.Books
+{
+    internal class BookMappingExtensions
+    {
+    }
+}
