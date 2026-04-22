324e087697b9e33532e4d500359e69bd00f78a0f
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-22 00:09:51 -0300

    update prince finalizado

---
+++ src/BookStore.Application/Features/Books/Commands/UpdateBookPrice/UpdateBookPriceCommand.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Application.Common.Messaging;
+using BookStore.Domain.Common;
+
+namespace BookStore.Application.Features.Books.Commands.UpdateBookPrice;
+
+public sealed record UpdateBookPriceCommand(
+    Guid BookId,
+    decimal NewPrice
+) : ICommand<Result>;
