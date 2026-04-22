b363a1103e92da6ffe8ce7fb57fd83b164e221f8
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-22 09:21:19 -0300

    delete book finalizado

---
+++ src/BookStore.Application/Features/Books/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,6 @@
+﻿using BookStore.Application.Common.Messaging;
+using BookStore.Domain.Common;
+
+namespace BookStore.Application.Features.Books.Commands.DeleteBook;
+
+public sealed record DeleteBookCommand(Guid BookId) : ICommand<Result>;
