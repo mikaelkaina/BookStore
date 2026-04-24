c1001c2d380935d2750419b4e693d943a0e91b50
Mikael Santos <mykaelsantos@outlook.com.br>
2026-04-24 01:39:42 -0300

    fechando o command de Costumer com o metodo delete

---
+++ src/BookStore.Application/Features/Customers/Commands/DeleteCustomer/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BookStore.Application.Features.Customers.Commands.DeleteCustomer;
+
+public sealed class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+{
+    public DeleteCustomerCommandValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .NotEmpty().WithMessage("CustomerId is required.");
+    }
+}
