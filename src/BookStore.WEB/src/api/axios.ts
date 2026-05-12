09d8d1d75c678b2f68ca35034e2dd469de207e1d
Mikael Santos <mykaelsantos@outlook.com.br>
2026-05-11 22:38:53 -0300

    refactor axios url

--- src/BookStore.WEB/src/api/axios.ts
+++ src/BookStore.WEB/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bookstore-f4btctchfmcvgzcu.brazilsouth-01.azurewebsites.net',
+  baseURL: 'https://bookstore-f4btctchfmcvgzcu.brazilsouth-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
